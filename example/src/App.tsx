fdff1012e9def717f08fa62a487e8b6bcc371517
Dwikavindra <dwikavindra.haryo.r@mail.ugm.ac.id>
2025-04-22 09:29:05 +0100

    Add package

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,25 +1,244 @@
-import { multiply } from 'upsell-background-actions';
-import { Text, View, StyleSheet } from 'react-native';
 import { useState, useEffect } from 'react';
+import { StyleSheet, View, Text, Button } from 'react-native';
+import BackgroundService from 'upsell-background-actions';
+import * as Sentry from '@sentry/react-native';
 
-export default function App() {
+Sentry.init({
+  dsn: 'https://3255bd38d055853e9b2a54d2c73af785@o4509191714701312.ingest.us.sentry.io/4509191719288832',
+  // Adds more context data to events (IP address, cookies, user, etc.)
+  // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
+  sendDefaultPii: true,
+});
+function App() {
   const [result, setResult] = useState<number | undefined>();
+  let process = 0;
+  const sleep = (time: any) =>
+    new Promise<void>((resolve) => setTimeout(() => resolve(), time));
 
-  useEffect(() => {
-    multiply(3, 7).then(setResult);
-  }, []);
+  // You can do anything in your task such as network requests, timers and so on,
+  // as long as it doesn't touch UI. Once your task completes (i.e. the promise is resolved),
+  // React Native will go into "paused" mode (unless there are other tasks running,
+  // or there is a foreground app).
+  const STOP_ALARM_MAX_RETRIES = 6;
+  const stopAlarm = async () => {
+    let i = 0;
+    while (i < STOP_ALARM_MAX_RETRIES) {
+      try {
+        await BackgroundService.stopAlarm();
+      } catch (error) {
+        const receivedError: Error = error as Error;
+        if (receivedError.message.includes('Pending Intent not Found')) {
+          console.log('This is error Pending Intent', receivedError);
+        }
+        if (receivedError.message.includes('Not Safe to stop Alarm')) {
+          console.log('This is error', receivedError);
+        }
+      }
+      i++;
+    }
+  };
+  const veryIntensiveTask = async (taskDataArguments: any) => {
+    // Example of an infinite loop task
+    let isError = false;
+    try {
+      console.log('Here in intensive task');
+      process = process + 1;
+      await BackgroundService.lock();
+      console.log('Passed lock');
+      for (
+        let i = 0;
+        await BackgroundService.isBackgroundServiceRunning();
+        i++
+      ) {
+        if (i === 10) {
+          throw new Error('Testing Error for restart');
+        }
+        console.log(
+          'This is await isBackgroundServiceRunning',
+          await BackgroundService.isBackgroundServiceRunning()
+        );
+        console.log('Number', i);
+        await sleep(1000);
+
+        // asleep longer than waiting time
+      }
+    } catch (e) {
+      console.log('Error from intensive', e);
+      isError = true;
+    } finally {
+      console.log(
+        'This is is list await ',
+        await BackgroundService.listRunningServices()
+      );
+      if (isError === true) {
+        await BackgroundService.sendStartServiceIntentInCatch();
+        console.log('Passed sendstartServiceInCatch');
+      }
+      console.log('Passed while loop');
+      await BackgroundService.unlock();
+      console.log('Passed unlock');
+      process = process - 1;
+      console.log('Process after unlock', process);
+    }
+  };
+
+  const options = {
+    taskName: 'Example',
+    taskTitle: 'ExampleTask title',
+    taskDesc: 'ExampleTask description',
+    taskIcon: {
+      name: 'ic_launcher',
+      type: 'mipmap',
+    },
+    color: '#ff00ff',
+    linkingURI: 'yourSchemeHere://chat/jane', // See Deep Linking for more info
+    parameters: {
+      delay: 1000,
+    },
+  };
+  // const restart1 = async () => {
+  //   try {
+  //     console.log('Here in restart 1');
+  //     await BackgroundService.setIsBackgroundServiceRunning(false);
+  //     await BackgroundService.sendStopBroadcast();
+  //     await BackgroundService.stopAlarm();
+  //     await sleep(5000);
+  //     console.log(
+  //       'This is running services',
+  //       await BackgroundService.listRunningServices()
+  //     );
+  //     await BackgroundService.start(veryIntensiveTask, options, 30000);
+  //   } catch (error) {
+  //     console.log('Restart1 error', error);
+  //   }
+  // };
 
   return (
     <View style={styles.container}>
+      <Button
+        title="running services "
+        onPress={async () => {
+          console.log(await BackgroundService.listRunningServices());
+        }}
+      />
+      <Button
+        title="stopAlarm"
+        onPress={async () => {
+          await BackgroundService.stopAlarm();
+        }}
+      />
+      <Button
+        title="Start Task "
+        onPress={async () => {
+          try {
+            BackgroundService.start(veryIntensiveTask, options, 5000);
+
+            console.log('Here After');
+          } catch (error) {
+            console.log('This is error', error);
+          }
+        }}
+      />
+      <Button
+        title="SetIsBackgroundServiceRunning False"
+        onPress={async () => {
+          try {
+            console.log('Here');
+            await BackgroundService.setIsBackgroundServiceRunning(false);
+            console.log('Here After');
+          } catch (error) {
+            console.log('This is error', error);
+          }
+        }}
+      />
+      <Button
+        title="Acquire AddPrinterSemaphore"
+        onPress={async () => {
+          try {
+            console.log('Here Before lockAddPrinterSemaphore');
+            await BackgroundService.lockAddPrinterSemaphore();
+            console.log('Here After lockAddPrinterSemaphore');
+          } catch (error) {
+            console.log('This is error', error);
+          }
+        }}
+      />
+      <Button
+        title="unlockAddPrinterSemaphorre"
+        onPress={async () => {
+          try {
+            console.log('Here before unlock addPrinterSemaphore');
+            await BackgroundService.unlockAddPrinterSemaphore();
+            console.log('Here After unlockAddPrinterSemaphore');
+          } catch (error) {
+            console.log('This is error', error);
+          }
+        }}
+      />
+      <Button
+        title="requestSchedulExactAlarmPermisssion"
+        onPress={async () => {
+          try {
+            await BackgroundService.requestExactAlarmPermission();
+          } catch (error) {
+            console.log('This is error', error);
+          }
+        }}
+      />
+      <Button
+        title="canScheduleExactAlarmPermission"
+        onPress={async () => {
+          try {
+            const result =
+              await BackgroundService.checkScheduleExactAlarmPermission();
+            console.log('This is canScheduleExactAlarm', result);
+          } catch (error) {
+            console.log('This is error', error);
+          }
+        }}
+      />
+      <Button
+        title="Stop Task "
+        onPress={async () => {
+          try {
+            console.log('Here in stop task');
+            try {
+              await BackgroundService.stopAlarm();
+            } catch (error) {
+              console.log('error from stopAlarm', error);
+            }
+
+            console.log('Passed Stopped Alarm');
+
+            await BackgroundService.setIsBackgroundServiceRunning(false);
+            console.log('Passed setIsBackgroundServiceRunnign');
+            await BackgroundService.sendStopBroadcast();
+            console.log('sendStopBroadcast');
+            await BackgroundService.unlock();
+            console.log('Passed unlock here ');
+            await BackgroundService.interruptQueuedThread();
+            console.log('Pass intteruptThread');
+            console.log('Passed sendStopBroadcast');
+          } catch (error) {
+            console.log('Error from stop task', error);
+          }
+        }}
+      />
+      <Button title="Test Sentry  " onPress={async () => {}} />
       <Text>Result: {result}</Text>
     </View>
   );
 }
-
+export default Sentry.wrap(App);
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  box: {
+    width: 60,
+    height: 60,
+    marginVertical: 20,
   },
 });
--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,157 @@
-import { NativeModules, Platform } from 'react-native';
+import { NativeModules, Platform, AppRegistry } from 'react-native';
 
-const LINKING_ERROR =
-  `The package 'upsell-background-actions' doesn't seem to be linked. Make sure: \n\n` +
-  Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
-  '- You rebuilt the app after installing the package\n' +
-  '- You are not using Expo Go\n';
+const { UpsellBackgroundActions } = NativeModules;
 
-const UpsellBackgroundActions = NativeModules.UpsellBackgroundActions
-  ? NativeModules.UpsellBackgroundActions
-  : new Proxy(
-      {},
-      {
-        get() {
-          throw new Error(LINKING_ERROR);
-        },
-      }
-    );
+class UpsellBackgroundService {
+  _runnedTasks: number;
+  _isRunning: boolean;
+  _currentOptions: any;
+  constructor() {
+    /** @private */
+    this._runnedTasks = 0;
+    /** @private @type {(arg0?: any) => void} */
+    /** @private */
+    this._isRunning = false;
+    /** @private @type {BackgroundTaskOptions} */
+    this._currentOptions;
+  }
 
-export function multiply(a: number, b: number): Promise<number> {
-  return UpsellBackgroundActions.multiply(a, b);
+  async updateNotification(taskData: any) {
+    if (Platform.OS !== 'android') return;
+    if (!this.isRunning())
+      throw new Error(
+        'A BackgroundAction must be running before updating the notification'
+      );
+    this._currentOptions = this._normalizeOptions({
+      ...this._currentOptions,
+      ...taskData,
+    });
+    await UpsellBackgroundActions.updateNotification(this._currentOptions);
+  }
+
+  /**
+   * Returns if the current background task is running.
+   *
+   * It returns `true` if `start()` has been called and the task has not finished.
+   *
+   * It returns `false` if `stop()` has been called, **even if the task has not finished**.
+   */
+  isRunning() {
+    return this._isRunning;
+  }
+  /**
+   * @argument {()=>{}} callback
+   * @returns {Promise<void>}
+   */
+  async setCallBack(callback: any) {
+    UpsellBackgroundActions.setCallBack(callback);
+  }
+
+  async start(task: any, options: { parameters: any }, triggerTime: number) {
+    this._runnedTasks++;
+    this._currentOptions = this._normalizeOptions(options);
+    const finalTask = this._generateTask(task, options.parameters);
+    if (Platform.OS === 'android') {
+      AppRegistry.registerHeadlessTask(
+        this._currentOptions.taskName,
+        () => finalTask
+      );
+      await UpsellBackgroundActions.start(this._currentOptions, triggerTime);
+      this._isRunning = true;
+    } else {
+      await UpsellBackgroundActions.start(this._currentOptions, triggerTime);
+      this._isRunning = true;
+      finalTask();
+    }
+  }
+
+  /**
+   * @private
+   * @template T
+   * @param {(taskData?: T) => Promise<void>} task
+   * @param {T} [parameters]
+   */
+  _generateTask(task: (arg0: any) => Promise<any>, parameters: any) {
+    return async () => {
+      await new Promise(() => {
+        task(parameters);
+      });
+    };
+  }
+  _normalizeOptions(options: any) {
+    return {
+      taskName: options.taskName + this._runnedTasks,
+      taskTitle: options.taskTitle,
+      taskDesc: options.taskDesc,
+      taskIcon: { ...options.taskIcon },
+      color: options.color || '#ffffff',
+      linkingURI: options.linkingURI,
+      progressBar: options.progressBar,
+    };
+  }
+
+  async sendStopBroadcast() {
+    await UpsellBackgroundActions.sendStopBroadcast();
+    this._isRunning = false;
+  }
+  async setAlarm(mili: number) {
+    return await UpsellBackgroundActions.setAlarm(mili);
+  }
+  async isBackgroundServiceRunning() {
+    return await UpsellBackgroundActions.isBackgroundServiceRunning();
+  }
+  async checkScheduleExactAlarmPermission() {
+    return await UpsellBackgroundActions.checkScheduleExactAlarmPermission();
+  }
+  async requestExactAlarmPermission() {
+    return await UpsellBackgroundActions.requestExactAlarmPermission();
+  }
+  async listRunningServices() {
+    return await UpsellBackgroundActions.listRunningServices();
+  }
+  async stopAlarm() {
+    return await UpsellBackgroundActions.stopAlarm();
+  }
+  async setIsBackgroundServiceRunning(value: boolean) {
+    console.log('Set Is backgroundServicerunningCalled');
+    return await UpsellBackgroundActions.setIsBackgroundServiceRunning(value);
+  }
+  async lock() {
+    return await UpsellBackgroundActions.lock();
+  }
+  async unlock() {
+    console.log('In release unlock');
+    return await UpsellBackgroundActions.unlock();
+  }
+  //TODO: Remove both later into its own semaphore package
+  async lockAddPrinterSemaphore() {
+    return await UpsellBackgroundActions.lockAddPrinterSemaphore();
+  }
+  async unlockAddPrinterSemaphore() {
+    return await UpsellBackgroundActions.unlockAddPrinterSemaphore();
+  }
+  async isIgnoreBatteryOptimization() {
+    return await UpsellBackgroundActions.isIgnoreBatteryOptimization();
+  }
+
+  async requestIgnoreBatteryOptmization() {
+    return await UpsellBackgroundActions.requestIgnoreBatteryOptmization();
+  }
+  async requestActionIgnoreBatteryOptimizationSettings() {
+    return await UpsellBackgroundActions.requestActionIgnoreBatteryOptimizationSettings();
+  }
+  async isItSafeToStopAlarm() {
+    return await UpsellBackgroundActions.getIsItSafeToStopAlarm();
+  }
+  async setIsAlarmStoppedByUser(value: boolean) {
+    return await UpsellBackgroundActions.setIsAlarmStoppedByUser(value);
+  }
+  async interruptQueuedThread() {
+    return await UpsellBackgroundActions.interruptQueuedThread();
+  }
+  async sendStartServiceIntentInCatch() {
+    return await UpsellBackgroundActions.sendStartServiceIntentInCatch();
+  }
 }
+const BackgroundService = new UpsellBackgroundService();
+export default BackgroundService;
